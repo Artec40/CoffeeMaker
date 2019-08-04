523535eb2222a76fd2728f6753bb6d9321374b3e
dnwdark <Artec40@gmail.com>
2019-08-04 23:55:58 +0500

    Добавлены объекты: кофе, чай, молоко. В объекте молоко не работает double Fat -1.5.

---
+++ src/CoffeeMaker/CoffeeMaker.Console/Milk.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeMaker.ConsoleApp
+{
+    /// <summary>
+    /// Это молоко.
+    /// </summary>
+    class Milk
+    {
+        /// <summary>
+        /// Жирность молока.
+        /// </summary>
+        public double Fat { get; set; }
+
+        /// <summary>
+        /// Инициализируем молоко.
+        /// </summary>
+        /// <param name="fat"> Значения для жирности молока. </param> 
+        public Milk(double fat)
+        {
+            this.Fat = fat;
+        }
+    }
+}
