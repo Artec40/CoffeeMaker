3698aa4e0a627241fb066eeed367248af2c99975
dnwdark <Artec40@gmail.com>
2019-08-05 22:03:28 +0500

    Изучил методы и свойства. Три слона - инкапсуляция, полиморфизм, наследование.

--- src/CoffeeMaker/CoffeeMaker.Console/Program.cs
+++ src/CoffeeMaker/CoffeeMaker.Console/Program.cs
@@ -2,23 +2,28 @@
 
 namespace CoffeeMaker.ConsoleApp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Coffee cupcoffee1 = new Coffee(100, 8);
-            Coffee cupcoffee2 = new Coffee(400, 3);
-            Console.WriteLine($"cup of coffee 1: {cupcoffee1.VolumeCoffee}, {cupcoffee1.Strong}.");
-            Console.WriteLine($"cup of coffee 2: {cupcoffee2.VolumeCoffee}, {cupcoffee2.Strong}.");
+            var drink1 = new Coffee(100, 8);
+            var drink2 = new Coffee(400, 3);
+            Console.WriteLine($"cup of coffee 1: {drink1.Volume}, {drink1.Strong}.");
+            Console.WriteLine($"cup of coffee 2: {drink2.Volume}, {drink2.Strong}.");
 
-            Tea cuptea1 = new Tea(300, "Black");
-            Tea cuptea2 = new Tea(300, "Green");
-            Console.WriteLine($"cup of tea1: {cuptea1.VolumeTea}, {cuptea1.Type}.");
-            Console.WriteLine($"cup of tea2: { cuptea2.VolumeTea}, {cuptea2.Type}.");
+            var cupoftea1 = new Tea(300, "Black");
+            var cupoftea2 = new Tea(300, "Green");
+            Console.WriteLine($"cup of tea1: {cupoftea1.Volume}, {cupoftea1.Type}.");
+            Console.WriteLine($"cup of tea2: {cupoftea2.Volume}, {cupoftea2.Type}.");
 
-            Milk milk = new Milk(1.5);
-            Console.WriteLine("Milk: ", milk.Fat);
+            var milk = new Milk(1.5);
+            Console.WriteLine($"Milk:, {milk.Fat}.");
 
+            var mouse = new Mouse();
+            Console.WriteLine(mouse.Roar(string.Empty));
+
+            var mouse2 = new LittleMouse();
+            Console.WriteLine(mouse2.Roar(string.Empty));
             Console.WriteLine("Hello world!");
         }
     }
